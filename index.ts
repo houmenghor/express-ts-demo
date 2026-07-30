a2226aed2d796f7c91de6a829e02a9aaa62fde2b
houmenghor <menghorhou@gmail.com>
2026-07-30 16:36:56 +0700

    feat: create, update and delete

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import express, {type Request, type Response} from 'express'
 
 const app = express();
-const port:number = 8080;
+app.use(express.json()); //convert from json obj to javascript obj
+const port:number = 8000;
 
 // interface UserParams {
 //     userId: string
@@ -31,7 +32,8 @@ const port:number = 8080;
 interface User {
     id: number,
     name: string,
-    gender: boolean
+    gender: boolean,
+    phone: string
 }
 
 interface UserIdParam {
@@ -39,16 +41,16 @@ interface UserIdParam {
 }
 
 const users: User[] = [
-    {
-        id: 1,
-        name: "Dara",
-        gender: true
-    } ,
-    {
-        id: 2,
-        name: "Sok",
-        gender: false
-    }
+    // {
+    //     id: 1,
+    //     name: "Dara",
+    //     gender: true
+    // } ,
+    // {
+    //     id: 2,
+    //     name: "Sok",
+    //     gender: false
+    // }
 ];
 
 app.get('/users', (req: Request, res: Response) => {
@@ -63,7 +65,7 @@ app.get('/users/:id', (req: Request<UserIdParam>, res: Response) => {
     const userId = Number(req.params.id);
     const user = users.find((user) => user.id === userId);
     if (!user) {
-        return res.json({
+        return res.status(404).json({
             status: false,
             message: 'User not found'
         })
@@ -74,6 +76,77 @@ app.get('/users/:id', (req: Request<UserIdParam>, res: Response) => {
         data: user
     })
 });
+
+interface CreateUserBody {
+    id: number,
+    name: string,
+    gender: boolean,
+    phone: string
+}
+
+interface SuccessResponse {
+    status: boolean,
+    message: string,
+    data: User
+}
+
+let userId:number = 0;
+app.post('/users', (req: Request<{}, any, CreateUserBody>, res: Response<SuccessResponse>) => {
+    userId++;
+    const newUser: User = {
+        id: userId,
+        name: req.body.name,
+        gender: req.body.gender,
+        phone: req.body.phone
+    }
+
+    users.push(newUser);
+    return res.status(201).json({
+        status: true,
+        message: 'Successfully created',
+        data: newUser
+    })
+});
+
+app.put('/users/:id', (req: Request<UserIdParam, any, CreateUserBody>, res: Response)=> {
+    const userId = Number(req.params.id);
+    const user = users.find((user) => user.id === userId);
+    // console.log(user);
+    if (!user) {
+        return res.status(404).json({
+            status: false,
+            message: 'User not found'
+        });
+    }
+
+    user.name = req.body.name;
+    user.gender = req.body.gender;
+    user.phone = req.body.phone;
+
+    return res.json({
+        status: true,
+        message: "User updated successfully!"
+    });
+});
+
+app.delete('/users/:id', (req: Request<UserIdParam, any>, res: Response) => {
+    const userId = Number(req.params.id);
+    const user = users.find((user) => user.id === userId);
+    // console.log(user);
+    if (!user) {
+        return res.status(404).json({
+            status: false,
+            message: 'User not found'
+        });
+    }
+    const index = users.indexOf(user);
+    users.splice(index, 1);
+    return res.json({
+        status: true,
+        message: 'Successfully deleted',
+    })
+});
+
 
 app.listen(port,() => {
      console.log(`Server running on port: ${port}`);
